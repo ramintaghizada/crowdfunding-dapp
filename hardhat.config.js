da3e2ac17823f9924b370f61202c2c3534c14864
Ramin Taghizada <ramintaghizada1@gmail.com>
2026-05-26 21:44:44 +0400

    add hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+// Updated hardhat.config.js
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
